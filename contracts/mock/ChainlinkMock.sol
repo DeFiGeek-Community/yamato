3c3282aa329552b806d2f5b1d751bfc25a00dbef
0xsomewherecat <0xsomewherecat@gmail.com>
2021-08-21 11:25:28 +0900

    Implement the Tellor Mock.

--- contracts/mock/ChainlinkMock.sol
+++ contracts/mock/ChainlinkMock.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.7.6;
 
+import "./OracleMockBase.sol";
+
 /*
  * SPDX-License-Identifier: GPL-3.0-or-later
  * Yamato
@@ -13,7 +15,7 @@ pragma solidity 0.7.6;
 //solhint-disable max-line-length
 //solhint-disable no-inline-assembly
 
-contract ChainlinkMock {
+contract ChainlinkMock is OracleMockBase {
     uint8 public symbol;
     uint8 private ETHUSD = 1;
     uint8 private JPYUSD = 2;
@@ -125,24 +127,6 @@ contract ChainlinkMock {
       require(timestamp != 0, "The specified round Id doesn't have a previous answer.");
       
       return (_roundId, prevAnswers[_roundId], timestamp, timestamp, prevAnsweredInRounds[_roundId]);
-    }
-
-    function randomize() private view returns (uint, bool) {
-      uint randomNumber = uint(keccak256(abi.encodePacked(msg.sender,  block.timestamp,  blockhash(block.number - 1))));
-      uint deviation = randomNumber % 11;
-      bool sign = randomNumber % 2 == 1 ? true : false;
-      return (deviation, sign);
-    }
-
-    // If chaos counter == 10, reset it to 0 and trigger chaos = 51% deviation
-    // Otherwise, increment the chaos counter and return false
-    function chaos() private returns (bool) {
-      if (chaosCounter == 10) {
-        chaosCounter == 0;
-        return true;
-      }
-      chaosCounter += 1;
-      return false;
     }
 
     function getRoundData(uint256 _roundId) public {}
---
+++ contracts/mock/OracleMockBase.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.7.6;
+
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Yamato
+ * Copyright (C) 2021 Yamato Protocol (DeFiGeek Community Japan)
+ *
+ * This Factory is a fork of Murray Software's deliverables.
+ * And this entire project is including the fork of Hegic Protocol.
+ * Hence the license is alinging to the GPL-3.0
+*/
+
+//solhint-disable max-line-length
+//solhint-disable no-inline-assembly
+
+// Base class to create a oracle mock contract for a specific provider
+contract OracleMockBase {
+
+    uint8 private chaosCounter = 0;
+
+    function randomize() internal view returns (uint, bool) {
+      uint randomNumber = uint(keccak256(abi.encodePacked(msg.sender,  block.timestamp,  blockhash(block.number - 1))));
+      uint deviation = randomNumber % 11;
+      bool sign = randomNumber % 2 == 1 ? true : false;
+      return (deviation, sign);
+    }
+
+    // If chaos counter == 10, reset it to 0 and trigger chaos = 51% deviation
+    // Otherwise, increment the chaos counter and return false
+    function chaos() internal returns (bool) {
+      if (chaosCounter == 10) {
+        chaosCounter == 0;
+        return true;
+      }
+      chaosCounter += 1;
+      return false;
+    }
+}
