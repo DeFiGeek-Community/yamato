0f84a8cf0976b56640e4df6de1bd273fc63c652e
Motoko Kusanagi <0xmotoko@pm.me>
2021-08-27 09:57:23 +0900

    Add more improvement to the cli:fetchPrice

--- src/priceFeedSDK.ts
+++ src/priceFeedSDK.ts
@@ -25,31 +25,60 @@ import {
     backToInitMode,
   } from '@src/deployUtil';
   
+const STATUS = [
+    'chainlinkWorking',
+    'usingTellorChainlinkUntrusted',
+    'bothOraclesUntrusted',
+    'usingTellorChainlinkFrozen',
+    'usingChainlinkTellorUntrusted'
+];
+
+
 (async ()=>{
     await setProvider();
 
     const feed: Contract = new Contract(PRICE_FEED_ADDR_RINKEBY, genABI("PriceFeed"), singletonProvider());
 
     console.log('calling')
-    let tx = await feed.connect(getFoundation()).fetchPrice({gasLimit: 300000, gasPrice:1*(1000**3)});
+    let tx = await feed.connect(getFoundation()).fetchPrice().catch(e=> console.trace(e.message) )
     console.log('waiting', tx.hash.substr(0,8))
     let res = await tx.wait();
 
 
-    let status = await feed.status().catch(console.trace);
-    let lastGoodPrice = await feed.lastGoodPrice().catch(console.trace);
+    let status = await feed.status().catch(e=> console.trace(e.message) )
+    let lastGoodPrice = await feed.lastGoodPrice().catch(e=> console.trace(e.message) )
+    let ethPriceAggregatorInUSD = await feed.ethPriceAggregatorInUSD().catch(e=> console.trace(e.message) )
+    let jpyPriceAggregatorInUSD = await feed.jpyPriceAggregatorInUSD().catch(e=> console.trace(e.message) )
+    let tellorCaller = await feed.tellorCaller().catch(e=> console.trace(e.message) )
 
-    let ethPriceAggregatorInUSD = await feed.ethPriceAggregatorInUSD().catch(console.trace);
-
-    console.log(`status:${status}, lastGoodPrice:${lastGoodPrice}, ethPriceAggregatorInUSD: ${ethPriceAggregatorInUSD}`);
+    console.log(`
+    status:${STATUS[status]},
+    lastGoodPrice:${lastGoodPrice},
+    ethPriceAggregatorInUSD: ${ethPriceAggregatorInUSD},
+    jpyPriceAggregatorInUSD: ${jpyPriceAggregatorInUSD},
+    tellorCaller: ${tellorCaller},
+    `);
 
     const EthPriceAggregatorInUSD: Contract = new Contract(ethPriceAggregatorInUSD, genABI("ChainLinkMock"), singletonProvider());
+    const JpyPriceAggregatorInUSD: Contract = new Contract(jpyPriceAggregatorInUSD, genABI("ChainLinkMock"), singletonProvider());
+    const TellorCaller: Contract = new Contract(tellorCaller, genABI("TellorCallerMock"), singletonProvider());
     
-    let tx2 = await EthPriceAggregatorInUSD.connect(getFoundation()).latestRoundData({gasLimit:100000})
-    let res2 = await tx2.wait();
+    console.log('cl1')
+    await ( await EthPriceAggregatorInUSD.connect(getFoundation()).latestRoundData({gasLimit: 100000}) ).wait();
+    console.log('cl2')
+    await ( await JpyPriceAggregatorInUSD.connect(getFoundation()).latestRoundData({gasLimit: 100000}) ).wait();
+    console.log('tl')
+    await ( await TellorCaller.connect(getFoundation()).getTellorCurrentValue(59, {gasLimit: 100000}) ).wait();
+    console.log('end')
 
-    let lastPrice = await EthPriceAggregatorInUSD.lastPrice()
+    let lastPriceEthPriceAggregatorInUSD = await EthPriceAggregatorInUSD.lastPrice()
+    let lastPriceJpyPriceAggregatorInUSD = await JpyPriceAggregatorInUSD.lastPrice()
+    let lastPriceTellorCaller = await TellorCaller.lastPrice()
 
-    console.log(`lastPrice:${lastPrice}`);
+    console.log(`
+    lastPriceEthPriceAggregatorInUSD:${lastPriceEthPriceAggregatorInUSD},
+    lastPriceJpyPriceAggregatorInUSD:${lastPriceJpyPriceAggregatorInUSD},
+    lastPriceTellorCaller:${lastPriceTellorCaller}
+    `);
 
 })().then();
