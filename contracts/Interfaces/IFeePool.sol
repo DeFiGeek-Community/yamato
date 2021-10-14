f3c18a8e3b04b01544e82ebdc97d9bee4d106067
Motoko Kusanagi <0xmotoko@pm.me>
2021-10-14 13:10:46 +0900

    Format again and again

--- contracts/Interfaces/IFeePool.sol
+++ contracts/Interfaces/IFeePool.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.8.4;
+
 /*
  * SPDX-License-Identifier: GPL-3.0-or-later
  * Written by 0xMotoko (0xmotoko@pm.me)
@@ -7,4 +8,6 @@ pragma solidity 0.8.4;
 
 //solhint-disable max-line-length
 //solhint-disable no-inline-assembly
-interface IFeePool {}
+interface IFeePool {
+
+}
