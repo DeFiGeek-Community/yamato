f411f7e609c8397d33ddd7a6cf05a41b20bb1b7e
Motoko Kusanagi <0xmotoko@pm.me>
2021-12-15 11:14:22 +0900

    Add lib

--- upgrade/005_upgrade_PriorityRegistry.ts
+++ upgrade/005_upgrade_PriorityRegistry.ts
@@ -3,7 +3,7 @@ import { runUpgrade } from "../src/upgradeUtil";
 const IMPL_NAME_BASE = "PriorityRegistry";
 
 async function main() {
-  await runUpgrade(IMPL_NAME_BASE);
+  await runUpgrade(IMPL_NAME_BASE, ["PledgeLib"]);
 }
 
 main().catch((e) => console.log(e));
