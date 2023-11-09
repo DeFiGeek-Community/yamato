1d9d63e0955c730c94612899b6d95d07c51f27dd
takadr <t4k4.cr@gmail.com>
2023-11-09 18:52:56 +0900

    Updated integration tests for VotingEscrow

--- test/fork/integration/VotingEscrow/depositWithdrawVoting.test.ts
+++ test/fork/integration/VotingEscrow/depositWithdrawVoting.test.ts
@@ -9,7 +9,8 @@ import {
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 
 const ACCOUNT_NUM = 10;
-const NUMBER_OF_ATTEMPTS = 30; // テストの試行回数
+const MAX_EXAMPLES = 30; // テストの試行回数
+const STATEFUL_STEP_COUNT = 20; // ruleの実行回数
 const YEAR = BigNumber.from(86400 * 365);
 const WEEK = BigNumber.from(86400 * 7);
 const two_to_the_256_minus_1 = BigNumber.from("2")
@@ -94,27 +95,27 @@ describe("VotingEscrow", function () {
     unlockTime = timestamp.add(WEEK.mul(stLockDuration)).div(WEEK).mul(WEEK);
 
     if (stValue.eq(0)) {
-      console.log("--revert: 1");
+      // console.log("--revert: 1");
       await expect(
         votingEscrow.connect(stAccount).createLock(stValue, unlockTime)
       ).to.revertedWith("need non-zero value");
     } else if (votingBalances[stAccountN]["value"].gt("0")) {
-      console.log("--revert: 2");
+      // console.log("--revert: 2");
       await expect(
         votingEscrow.connect(stAccount).createLock(stValue, unlockTime)
       ).to.revertedWith("Withdraw old tokens first");
     } else if (unlockTime.lte(timestamp)) {
-      console.log("--revert: 3");
+      // console.log("--revert: 3");
       await expect(
         votingEscrow.connect(stAccount).createLock(stValue, unlockTime)
       ).to.revertedWith("Can only lock until time in the future");
     } else if (unlockTime.gte(timestamp.add(YEAR.mul("4")))) {
-      console.log("--revert: 4");
+      // console.log("--revert: 4");
       await expect(
         votingEscrow.connect(stAccount).createLock(stValue, unlockTime)
       ).to.revertedWith("Voting lock can be 4 years max");
     } else {
-      console.log("--success, account:", stAccountN);
+      // console.log("--success, account:", stAccountN);
       const tx = await votingEscrow
         .connect(stAccount)
         .createLock(stValue, unlockTime);
@@ -140,17 +141,17 @@ describe("VotingEscrow", function () {
     let timestamp = BigNumber.from(await time.latest());
 
     if (stValue.eq(0)) {
-      console.log("--revert: 1");
+      // console.log("--revert: 1");
       await expect(
         votingEscrow.connect(stAccount).increaseAmount(stValue)
       ).to.revertedWith("dev: need non-zero value");
     } else if (votingBalances[stAccountN]["value"].eq("0")) {
-      console.log("--revert: 2");
+      // console.log("--revert: 2");
       await expect(
         votingEscrow.connect(stAccount).increaseAmount(stValue)
       ).to.revertedWith("No existing lock found");
     } else if (votingBalances[stAccountN]["unlockTime"].lte(timestamp)) {
-      console.log("--revert: 3");
+      // console.log("--revert: 3");
       await expect(
         votingEscrow.connect(stAccount).increaseAmount(stValue)
       ).to.revertedWith("Cannot add to expired lock. Withdraw");
@@ -159,12 +160,12 @@ describe("VotingEscrow", function () {
       votingBalances[stAccountN]["value"] =
         votingBalances[stAccountN]["value"].add(stValue);
 
-      console.log(
-        "--success, account:",
-        stAccountN,
-        "new balance:",
-        votingBalances[stAccountN]["value"].toString()
-      );
+      // console.log(
+      //   "--success, account:",
+      //   stAccountN,
+      //   "new balance:",
+      //   votingBalances[stAccountN]["value"].toString()
+      // );
     }
   }
 
@@ -185,27 +186,27 @@ describe("VotingEscrow", function () {
       .mul(WEEK);
 
     if (votingBalances[stAccountN]["unlockTime"].lte(timestamp)) {
-      console.log("--revert: 1");
+      // console.log("--revert: 1");
       await expect(
         votingEscrow.connect(stAccount).increaseUnlockTime(unlockTime)
       ).to.revertedWith("Lock expired");
     } else if (votingBalances[stAccountN]["value"].eq("0")) {
-      console.log("--revert: 2");
+      // console.log("--revert: 2");
       await expect(
         votingEscrow.connect(stAccount).increaseUnlockTime(unlockTime)
       ).to.revertedWith("Nothing is locked");
     } else if (votingBalances[stAccountN]["unlockTime"].gte(unlockTime)) {
-      console.log("--revert: 3");
+      // console.log("--revert: 3");
       await expect(
         votingEscrow.connect(stAccount).increaseUnlockTime(unlockTime)
       ).to.revertedWith("Can only increase lock duration");
     } else if (unlockTime.gt(timestamp.add(YEAR.mul("4")))) {
-      console.log("--revert: 4");
+      // console.log("--revert: 4");
       await expect(
         votingEscrow.connect(stAccount).increaseUnlockTime(unlockTime)
       ).to.revertedWith("Voting lock can be 4 years max");
     } else {
-      console.log("--success, account:", stAccountN);
+      // console.log("--success, account:", stAccountN);
       const tx = await votingEscrow
         .connect(stAccount)
         .increaseUnlockTime(unlockTime);
@@ -227,12 +228,12 @@ describe("VotingEscrow", function () {
     let timestamp = BigNumber.from(await time.latest());
 
     if (votingBalances[stAccountN]["unlockTime"].gt(timestamp)) {
-      console.log("--reverted");
+      // console.log("--reverted");
       await expect(votingEscrow.connect(stAccount).withdraw()).to.revertedWith(
         "The lock didn't expire"
       );
     } else {
-      console.log("--success, account:", stAccountN);
+      // console.log("--success, account:", stAccountN);
       await votingEscrow.connect(stAccount).withdraw();
       votingBalances[stAccountN]["value"] = BigNumber.from("0");
     }
@@ -263,38 +264,17 @@ describe("VotingEscrow", function () {
     }
   }
 
-  let func = [
-    "ruleCreateLock",
-    "ruleIncreaseAmount",
-    "ruleIncreaseUnlockTime",
-    "ruleWithdraw",
-    "ruleCheckpoint",
-    "ruleAdvanceTime",
-  ];
+  async function checkInvariants() {
+    // console.log("=====Invariant checks=====");
 
-  describe("test_deposit_withdraw_voting", function () {
-    //set arbitral number of repeats
-    for (let x = 0; x < NUMBER_OF_ATTEMPTS; x++) {
-      it("try " + eval("x+1"), async () => {
-        for (let i = 0; i < 100; i++) {
-          let n = (await rdmValue(func.length)).toNumber();
-          await eval(func[n])();
-        }
-      });
-    }
-  });
-
-  afterEach(async () => {
-    console.log("=====Invariant checks=====");
-
-    console.log("invariant_token_balances");
+    // console.log("invariant_token_balances");
     for (let i = 0; i < ACCOUNT_NUM; i++) {
       expect(await token.balanceOf(accounts[i].address)).to.equal(
         ten_to_the_40.sub(votingBalances[i]["value"])
       );
     }
 
-    console.log("invariant_escrow_current_balances");
+    // console.log("invariant_escrow_current_balances");
     let total_supply = BigNumber.from("0");
     let timestamp = BigNumber.from(await time.latest());
 
@@ -314,10 +294,10 @@ describe("VotingEscrow", function () {
     }
     expect(await votingEscrow["totalSupply()"]()).to.equal(total_supply);
 
-    console.log("invariant_historic_balances");
+    // console.log("invariant_historic_balances");
     total_supply = BigNumber.from("0");
     let blocknumber = (await time.latestBlock()) - 4;
-    console.log(blocknumber);
+    // console.log(blocknumber);
 
     for (let i = 0; i < ACCOUNT_NUM; i++) {
       total_supply = total_supply.add(
@@ -328,7 +308,32 @@ describe("VotingEscrow", function () {
     expect(await votingEscrow.totalSupplyAt(blocknumber)).to.equal(
       total_supply
     );
+  }
 
+  let func = [
+    "ruleCreateLock",
+    "ruleIncreaseAmount",
+    "ruleIncreaseUnlockTime",
+    "ruleWithdraw",
+    "ruleCheckpoint",
+    "ruleAdvanceTime",
+  ];
+
+  describe("test_deposit_withdraw_voting", function () {
+    //set arbitral number of repeats
+    for (let x = 0; x < MAX_EXAMPLES; x++) {
+      it(`Try ${x}`, async () => {
+        let steps = Math.floor(Math.random() * (STATEFUL_STEP_COUNT - 1)) + 1;
+        for (let i = 0; i < steps; i++) {
+          let n = (await rdmValue(func.length)).toNumber();
+          await eval(func[n])();
+          await checkInvariants();
+        }
+      });
+    }
+  });
+
+  afterEach(async () => {
     await snapshot.restore();
   });
 });
