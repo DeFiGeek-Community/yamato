f44fe670812fdb9d246bf63584282c6c2f2b0e70
naizo10 <naizo018767@gmail.com>
2023-11-29 16:40:30 +0900

    Removed type_id on controllers to make it only currency

--- contracts/ScoreWeightController.sol
+++ contracts/ScoreWeightController.sol
@@ -33,14 +33,6 @@ contract ScoreWeightController is UUPSBase {
         uint256 end;
     }
 
-    event AddType(string name, int128 typeId);
-    event NewTypeWeight(
-        int128 typeId,
-        uint256 time,
-        uint256 weight,
-        uint256 totalWeight
-    );
-
     event NewScoreWeight(
         address scoreAddress,
         uint256 time,
@@ -53,7 +45,7 @@ contract ScoreWeightController is UUPSBase {
         address scoreAddr,
         uint256 weight
     );
-    event NewScore(address addr, int128 scoreType, uint256 weight);
+    event NewScore(address addr, uint256 weight);
 
     uint256 constant MULTIPLIER = 10 ** 18;
 
@@ -62,16 +54,10 @@ contract ScoreWeightController is UUPSBase {
     // Voting escrow
     address public votingEscrow;
 
-    int128 public nScoreTypes;
     int128 public nScores; //number of scores
-    mapping(int128 => string) public scoreTypeNames;
 
     // Needed for enumeration
     mapping(int128 => address) public scores;
-
-    // we increment values by 1 prior to storing them here so we can rely on a value
-    // of zero as meaning the score has not been set    mapping(address => int128) scoreTypes;
-    mapping(address => int128) public scoreTypes_;
 
     mapping(address => mapping(address => VotedSlope)) public voteUserSlopes; // user -> score_addr -> VotedSlope
 
@@ -88,15 +74,8 @@ contract ScoreWeightController is UUPSBase {
     mapping(address => mapping(uint256 => uint256)) public changesWeight; // score_addr -> time -> slope
     mapping(address => uint256) public timeWeight; // score_addr -> last scheduled time (next week)
 
-    mapping(int128 => mapping(uint256 => Point)) public pointsSum; // type_id -> time -> Point
-    mapping(int128 => mapping(uint256 => uint256)) public changesSum; // type_id -> time -> slope
-    mapping(int128 => uint256) public timeSum; // type_id -> last scheduled time (next week)
-
     mapping(uint256 => uint256) public pointsTotal; // time -> total weight
     uint256 public timeTotal; // last scheduled time
-
-    mapping(int128 => mapping(uint256 => uint256)) public pointsTypeWeight; // type_id -> time -> type weight
-    mapping(int128 => uint256) public timeTypeWeight; // type_id -> last scheduled time (next week)
 
     /***
      *@notice Contract constructor
@@ -117,135 +96,19 @@ contract ScoreWeightController is UUPSBase {
     }
 
     /***
-     *@notice Get score type for address
-     *@param addr_ Score address
-     *@return Score type id
-     */
-    function scoreTypes(address addr_) external view returns (int128) {
-        int128 _scoreType = scoreTypes_[addr_];
-        require(_scoreType != 0, "dev: score is not added");
-
-        return _scoreType - 1;
-    }
-
-    /***
-     *@notice Fill historic type weights week-over-week for missed checkins
-     *        and return the type weight for the future week
-     *@param _score_type Score type id
-     *@return Type weight
-     */
-    function _getTypeWeight(int128 scoreType_) internal returns (uint256) {
-        uint256 _t = timeTypeWeight[scoreType_];
-        if (_t > 0) {
-            uint256 _w = pointsTypeWeight[scoreType_][_t];
-            for (uint256 i; i < 500; ) {
-                if (_t > block.timestamp) {
-                    break;
-                }
-                _t += WEEK;
-                pointsTypeWeight[scoreType_][_t] = _w;
-                if (_t > block.timestamp) {
-                    timeTypeWeight[scoreType_] = _t;
-                }
-                unchecked {
-                    ++i;
-                }
-            }
-            return _w;
-        } else {
-            return 0;
-        }
-    }
-
-    /***
-     *@notice Fill sum of score weights for the same type week-over-week for
-     *        missed checkins and return the sum for the future week
-     *@param scoreType_ Score type id
-     *@return Sum of weights
-     */
-    function _getSum(int128 scoreType_) internal returns (uint256) {
-        uint256 _t = timeSum[scoreType_];
-        if (_t > 0) {
-            Point memory _pt = pointsSum[scoreType_][_t];
-            for (uint256 i; i < 500; ) {
-                if (_t > block.timestamp) {
-                    break;
-                }
-                _t += WEEK;
-                uint256 _dBias = _pt.slope * WEEK;
-                if (_pt.bias > _dBias) {
-                    _pt.bias -= _dBias;
-                    uint256 _dSlope = changesSum[scoreType_][_t];
-                    _pt.slope -= _dSlope;
-                } else {
-                    _pt.bias = 0;
-                    _pt.slope = 0;
-                }
-                pointsSum[scoreType_][_t] = _pt;
-                if (_t > block.timestamp) {
-                    timeSum[scoreType_] = _t;
-                }
-                unchecked {
-                    ++i;
-                }
-            }
-            return _pt.bias;
-        } else {
-            return 0;
-        }
-    }
-
-    /***
      *@notice Fill historic total weights week-over-week for missed checkins
      *        and return the total for the future week
      *@return Total weight
      */
     function _getTotal() internal returns (uint256) {
         uint256 _t = timeTotal;
-        int128 _nScoreTypes = nScoreTypes;
         if (_t > block.timestamp) {
             // If we have already checkpointed - still need to change the value
             _t -= WEEK;
         }
         uint256 _pt = pointsTotal[_t];
 
-        for (int128 _scoreType; _scoreType < 100; ) {
-            if (_scoreType == _nScoreTypes) {
-                break;
-            }
-            _getSum(_scoreType);
-            _getTypeWeight(_scoreType);
-            unchecked {
-                ++_scoreType;
-            }
-        }
-        for (uint256 i; i < 500; ) {
-            if (_t > block.timestamp) {
-                break;
-            }
-            _t += WEEK;
-            _pt = 0;
-            // Scales as n_types * n_unchecked_weeks (hopefully 1 at most)
-            for (int128 _scoreType; _scoreType < 100; ) {
-                if (_scoreType == _nScoreTypes) {
-                    break;
-                }
-                uint256 _typeSum = pointsSum[_scoreType][_t].bias;
-                uint256 _typeWeight = pointsTypeWeight[_scoreType][_t];
-                _pt += _typeSum * _typeWeight;
-                unchecked {
-                    ++_scoreType;
-                }
-            }
-            pointsTotal[_t] = _pt;
 
-            if (_t > block.timestamp) {
-                timeTotal = _t;
-            }
-            unchecked {
-                ++i;
-            }
-        }
         return _pt;
     }
 
@@ -290,47 +153,33 @@ contract ScoreWeightController is UUPSBase {
     /***
      *@notice Add Currency `addr` of type `score_type` with weight `weight`
      *@param addr_ Score address
-     *@param scoreType_ Score type
+     *@param weight_ Score weight
      */
     function addCurrency(
         address addr_,
-        int128 scoreType_,
         uint256 weight_
     ) external onlyGovernance {
-        require(
-            (scoreType_ >= 0) && (scoreType_ < nScoreTypes),
-            "score_type 0 means unset"
-        ); //score_type 0 means unset
-        require(scoreTypes_[addr_] == 0, "cannot add the same score twice");
         int128 _n = nScores;
         unchecked {
             nScores = _n + 1;
         }
         scores[_n] = addr_;
-        scoreTypes_[addr_] = scoreType_ + 1;
         uint256 _nextTime;
         unchecked {
             _nextTime = ((block.timestamp + WEEK) / WEEK) * WEEK;
         }
 
         if (weight_ > 0) {
-            uint256 _typeWeight = _getTypeWeight(scoreType_);
-            uint256 _oldSum = _getSum(scoreType_);
             uint256 _oldTotal = _getTotal();
 
-            pointsSum[scoreType_][_nextTime].bias = weight_ + _oldSum;
-            timeSum[scoreType_] = _nextTime;
-            pointsTotal[_nextTime] = _oldTotal + (_typeWeight * weight_);
+            pointsTotal[_nextTime] = _oldTotal + weight_;
             timeTotal = _nextTime;
 
             pointsWeight[addr_][_nextTime].bias = weight_;
         }
-        if (timeSum[scoreType_] == 0) {
-            timeSum[scoreType_] = _nextTime;
-        }
         timeWeight[addr_] = _nextTime;
 
-        emit NewScore(addr_, scoreType_, weight_);
+        emit NewScore(addr_, weight_);
     }
 
     /***
@@ -365,11 +214,9 @@ contract ScoreWeightController is UUPSBase {
         uint256 _totalWeight = pointsTotal[_t];
 
         if (_totalWeight > 0) {
-            int128 _scoreType = scoreTypes_[addr_] - 1;
-            uint256 _typeWeight = pointsTypeWeight[_scoreType][_t];
             uint256 _scoreWeight = pointsWeight[addr_][_t].bias;
 
-            return (MULTIPLIER * _typeWeight * _scoreWeight) / _totalWeight;
+            return (MULTIPLIER * _scoreWeight) / _totalWeight;
         } else {
             return 0;
         }
@@ -409,71 +256,10 @@ contract ScoreWeightController is UUPSBase {
         return _scoreRelativeWeight(addr_, time_);
     }
 
-    /***
-     *@notice Change type weight
-     *@param typeId_ Type id
-     *@param weight_ New type weight
-     */
-    function _changeTypeWeight(int128 typeId_, uint256 weight_) internal {
-        uint256 _oldWeight = _getTypeWeight(typeId_);
-        uint256 _oldSum = _getSum(typeId_);
-        uint256 _totalWeight = _getTotal();
-        uint256 _nextTime;
-        unchecked {
-            _nextTime = ((block.timestamp + WEEK) / WEEK) * WEEK;
-        }
-
-        _totalWeight =
-            _totalWeight +
-            (_oldSum * weight_) -
-            (_oldSum * _oldWeight);
-        pointsTotal[_nextTime] = _totalWeight;
-        pointsTypeWeight[typeId_][_nextTime] = weight_;
-        timeTotal = _nextTime;
-        timeTypeWeight[typeId_] = _nextTime;
-
-        emit NewTypeWeight(typeId_, _nextTime, weight_, _totalWeight);
-    }
-
-    /***
-     *@notice Add score type with name `name_` and weight `weight`　//ex. type=1, Liquidity, 1*1e18
-     *@param name_ Name of score type
-     *@param weight_ Weight of score type
-     */
-    function addType(
-        string memory name_,
-        uint256 weight_
-    ) external onlyGovernance {
-        int128 _typeId = nScoreTypes;
-        scoreTypeNames[_typeId] = name_;
-        unchecked {
-            nScoreTypes = _typeId + 1;
-        }
-        if (weight_ != 0) {
-            _changeTypeWeight(_typeId, weight_);
-            emit AddType(name_, _typeId);
-        }
-    }
-
-    /***
-     *@notice Change score type `type_id` weight to `weight`
-     *@param typeId_ Score type id
-     *@param weight_ New Score weight
-     */
-    function changeTypeWeight(
-        int128 typeId_,
-        uint256 weight_
-    ) external onlyGovernance {
-        _changeTypeWeight(typeId_, weight_);
-    }
-
     function _changeScoreWeight(address addr_, uint256 weight_) internal {
         // Change score weight
         // Only needed when testing in reality
-        int128 _scoreType = scoreTypes_[addr_] - 1;
         uint256 _oldScoreWeight = _getWeight(addr_);
-        uint256 _typeWeight = _getTypeWeight(_scoreType);
-        uint256 _oldSum = _getSum(_scoreType);
         uint256 _totalWeight = _getTotal();
         uint256 _nextTime;
         unchecked {
@@ -483,14 +269,8 @@ contract ScoreWeightController is UUPSBase {
         pointsWeight[addr_][_nextTime].bias = weight_;
         timeWeight[addr_] = _nextTime;
 
-        uint256 newSum = _oldSum + weight_ - _oldScoreWeight;
-        pointsSum[_scoreType][_nextTime].bias = newSum;
-        timeSum[_scoreType] = _nextTime;
-
         _totalWeight =
-            _totalWeight +
-            (newSum * _typeWeight) -
-            (_oldSum * _typeWeight);
+            _totalWeight + weight_ - _oldScoreWeight;
         pointsTotal[_nextTime] = _totalWeight;
         timeTotal = _nextTime;
 
@@ -515,7 +295,6 @@ contract ScoreWeightController is UUPSBase {
         uint256 lockEnd;
         uint256 _nScores;
         uint256 nextTime;
-        int128 scoreType;
         uint256 oldDt;
         uint256 oldBias;
     }
@@ -551,8 +330,6 @@ contract ScoreWeightController is UUPSBase {
             );
         }
 
-        _vp.scoreType = scoreTypes_[scoreAddr_] - 1;
-        require(_vp.scoreType >= 0, "Score not added");
         // Prepare slopes and biases in memory
         VotedSlope memory _oldSlope = voteUserSlopes[msg.sender][scoreAddr_];
         _vp.oldDt = 0;
@@ -582,34 +359,23 @@ contract ScoreWeightController is UUPSBase {
         // Schedule recording of initial slope for nextTime
         uint256 _oldWeightBias = _getWeight(scoreAddr_);
         uint256 _oldWeightSlope = pointsWeight[scoreAddr_][_vp.nextTime].slope;
-        uint256 _oldSumBias = _getSum(_vp.scoreType);
-        uint256 _oldSumSlope = pointsSum[_vp.scoreType][_vp.nextTime].slope;
 
         pointsWeight[scoreAddr_][_vp.nextTime].bias =
             max(_oldWeightBias + _newBias, _vp.oldBias) -
-            _vp.oldBias;
-        pointsSum[_vp.scoreType][_vp.nextTime].bias =
-            max(_oldSumBias + _newBias, _vp.oldBias) -
             _vp.oldBias;
         if (_oldSlope.end > _vp.nextTime) {
             pointsWeight[scoreAddr_][_vp.nextTime].slope =
                 max(_oldWeightSlope + _newSlope.slope, _oldSlope.slope) -
                 _oldSlope.slope;
-            pointsSum[_vp.scoreType][_vp.nextTime].slope =
-                max(_oldSumSlope + _newSlope.slope, _oldSlope.slope) -
-                _oldSlope.slope;
         } else {
             pointsWeight[scoreAddr_][_vp.nextTime].slope += _newSlope.slope;
-            pointsSum[_vp.scoreType][_vp.nextTime].slope += _newSlope.slope;
         }
         if (_oldSlope.end > block.timestamp) {
             // Cancel old slope changes if they still didn't happen
             changesWeight[scoreAddr_][_oldSlope.end] -= _oldSlope.slope;
-            changesSum[_vp.scoreType][_oldSlope.end] -= _oldSlope.slope;
         }
         // Add slope changes for new slopes
         changesWeight[scoreAddr_][_newSlope.end] += _newSlope.slope;
-        changesSum[_vp.scoreType][_newSlope.end] += _newSlope.slope;
 
         _getTotal();
 
@@ -631,31 +397,11 @@ contract ScoreWeightController is UUPSBase {
     }
 
     /***
-     *@notice Get current type weight
-     *@param typeId_ Type id
-     *@return Type weight
-     */
-    function getTypeWeight(int128 typeId_) external view returns (uint256) {
-        return pointsTypeWeight[typeId_][timeTypeWeight[typeId_]];
-    }
-
-    /***
      *@notice Get current total (type-weighted) weight
      *@return Total weight
      */
     function getTotalWeight() external view returns (uint256) {
         return pointsTotal[timeTotal];
-    }
-
-    /***
-     *@notice Get sum of score weights per type
-     *@param typeId_ Type id
-     *@return Sum of score weights
-     */
-    function getWeightsSumPerType(
-        int128 typeId_
-    ) external view returns (uint256) {
-        return pointsSum[typeId_][timeSum[typeId_]].bias;
     }
 
     function max(uint256 _a, uint256 _b) internal pure returns (uint256) {
