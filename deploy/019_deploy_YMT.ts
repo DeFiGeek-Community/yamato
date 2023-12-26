06a46d45dd05c6f98c8e4279841f3bd335467fe5
naizo10 <naizo018767@gmail.com>
2023-12-26 13:44:33 +0900

    Add deployment script for YMT contract

---
+++ deploy/019_deploy_YMT.ts
@@ -0,0 +1,22 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { deploy, setNetwork, setProvider } from "../src/deployUtil";
+import { getDeploymentAddressPath } from "../src/deployUtil";
+import { existsSync } from "fs";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (existsSync(getDeploymentAddressPath("YMT"))) return;
+
+  setNetwork(hre.network.name);
+  const p = await setProvider();
+  const { ethers, deployments } = hre;
+  const { getContractFactory } = ethers;
+
+  await deploy("YMT", {
+    args: [],
+    getContractFactory,
+    deployments,
+  }).catch((e) => console.trace(e.message));
+};
+export default func;
+func.tags = ["YMT"];
