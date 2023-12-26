1558e4bb6922a0d1290e82786f1bb753362db5ea
naizo10 <naizo018767@gmail.com>
2023-12-26 15:58:17 +0900

    Add deploy function for veYMT contract

---
+++ deploy/020_deploy_veYMT.ts
@@ -0,0 +1,25 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { deploy, setNetwork, setProvider } from "../src/deployUtil";
+import { getDeploymentAddressPath } from "../src/deployUtil";
+import { readFileSync, existsSync } from "fs";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (existsSync(getDeploymentAddressPath("veYMT"))) return;
+  if (existsSync(getDeploymentAddressPath("YMT"))) return;
+
+  setNetwork(hre.network.name);
+  const p = await setProvider();
+  const { ethers, deployments } = hre;
+  const { getContractFactory } = ethers;
+
+  const ymtAddr = readFileSync(getDeploymentAddressPath("YMT")).toString();
+
+  await deploy("veYMT", {
+    args: [ymtAddr],
+    getContractFactory,
+    deployments,
+  }).catch((e) => console.trace(e.message));
+};
+export default func;
+func.tags = ["veYMT"];
