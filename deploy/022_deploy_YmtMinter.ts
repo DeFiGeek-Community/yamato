35c0cead7cbd91795e6152ccb957413e6adac08a
naizo10 <naizo018767@gmail.com>
2023-12-27 12:15:24 +0900

    Add YmtMinter deployment script

---
+++ deploy/022_deploy_YmtMinter.ts
@@ -0,0 +1,51 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import {
+  deploy,
+  setProvider,
+  getDeploymentAddressPath,
+  getDeploymentAddressPathWithTag,
+  setNetwork,
+} from "../src/deployUtil";
+import { readFileSync, writeFileSync, existsSync } from "fs";
+import { YmtMinter, YmtMinter__factory } from "../typechain";
+import { getProxy } from "../src/testUtil";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (existsSync(getDeploymentAddressPathWithTag("YmtMinter", "ERC1967Proxy")))
+    return;
+  if (
+    existsSync(
+      getDeploymentAddressPathWithTag("ScoreWeightController", "ERC1967Proxy")
+    )
+  )
+    return;
+  if (existsSync(getDeploymentAddressPath("YMT"))) return;
+
+  setNetwork(hre.network.name);
+  const p = await setProvider();
+  const { ethers, deployments } = hre;
+  const { getContractFactory } = ethers;
+
+  const ymtAddr = readFileSync(getDeploymentAddressPath("YMT")).toString();
+  const controllerAddr = readFileSync(
+    getDeploymentAddressPathWithTag("ScoreWeightController", "ERC1967Proxy")
+  ).toString();
+
+  const inst = await getProxy<YmtMinter, YmtMinter__factory>("YmtMinter", [
+    ymtAddr,
+    controllerAddr,
+  ]);
+  const implAddr = await inst.getImplementation();
+
+  writeFileSync(
+    getDeploymentAddressPathWithTag("YmtMinter", "ERC1967Proxy"),
+    inst.address
+  );
+  writeFileSync(
+    getDeploymentAddressPathWithTag("YmtMinter", "UUPSImpl"),
+    implAddr
+  );
+};
+export default func;
+func.tags = ["YmtMinter"];
