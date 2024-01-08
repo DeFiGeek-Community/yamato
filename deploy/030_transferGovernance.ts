810341d099064964935d1b9d0c6e855b73683133
naizo10 <naizo018767@gmail.com>
2024-01-08 12:19:41 +0900

    Add transfer governance functionality to deployment

---
+++ deploy/030_transferGovernance.ts
@@ -0,0 +1,69 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import {
+  setProvider,
+  getDeploymentAddressPath,
+  getDeploymentAddressPathWithTag,
+  getFoundation,
+  getMultisigGoverner,
+  setNetwork,
+} from "../src/deployUtil";
+import { readFileSync } from "fs";
+import { genABI } from "../src/genABI";
+import { Contract } from "ethers";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const multisigAddr = process.env.UUPS_PROXY_ADMIN_MULTISIG_ADDRESS;
+  if (!multisigAddr) return;
+
+  setNetwork(hre.network.name);
+  const p = await setProvider();
+
+  const _YMTAddr = readFileSync(getDeploymentAddressPath("YMT")).toString();
+  const YMT = new Contract(_YMTAddr, genABI("YMT"), p);
+
+  const _ymtVestingAddr = readFileSync(
+    getDeploymentAddressPath("YmtVesting")
+  ).toString();
+  const YmtVesting = new Contract(_ymtVestingAddr, genABI("YmtVesting"), p);
+
+  const _controllerAddr = readFileSync(
+    getDeploymentAddressPath("ScoreWeightController")
+  ).toString();
+  const ScoreWeightController = new Contract(
+    _controllerAddr,
+    genABI("ScoreWeightController"),
+    p
+  );
+
+  const _scoreRegistryAddr = readFileSync(
+    getDeploymentAddressPathWithTag("ScoreRegistry", "ERC1967Proxy")
+  ).toString();
+  const ScoreRegistry = new Contract(
+    _scoreRegistryAddr,
+    genABI("ScoreRegistry"),
+    p
+  );
+
+  await (await YMT.connect(getFoundation()).setAdmin(multisigAddr)).wait();
+  console.log(`log: YMT.setAdmin(${multisigAddr}) executed.`);
+  await (
+    await YmtVesting.connect(getFoundation()).setAdmin(multisigAddr)
+  ).wait();
+  console.log(`log: YmtVesting.setAdmin(${multisigAddr}) executed.`);
+  await (
+    await ScoreWeightController.connect(getFoundation()).setGovernance(
+      multisigAddr
+    )
+  ).wait();
+  console.log(
+    `log: ScoreWeightController.setGovernance(${multisigAddr}) executed.`
+  );
+  await (
+    await ScoreRegistry.connect(getFoundation()).setGovernance(multisigAddr)
+  ).wait();
+  console.log(`log: ScoreRegistry.setGovernance(${multisigAddr}) executed.`);
+};
+
+export default func;
+func.tags = ["transferGovernance"];
