d51e7e7d3e0e0c14593dc52de00121afdb00855b
naizo10 <naizo018767@gmail.com>
2024-01-12 23:27:43 +0900

    Refactor event extraction and save function

--- scripts/extractAndSaveEvents.ts
+++ scripts/extractAndSaveEvents.ts
@@ -12,13 +12,27 @@ interface EventData {
 
 function readEventJsonFiles(): EventData {
   const events: EventData = {
-    depositedEvents: JSON.parse(readFileSync(`./scripts/events/deposited.json`, 'utf-8')),
-    borrowedEvents: JSON.parse(readFileSync(`./scripts/events/borrowed.json`, 'utf-8')),
-    repaidEvents: JSON.parse(readFileSync(`./scripts/events/repaid.json`, 'utf-8')),
-    withdrawnEvents: JSON.parse(readFileSync(`./scripts/events/withdrawn.json`, 'utf-8')),
-    redeemedEvents: JSON.parse(readFileSync(`./scripts/events/redeemed.json`, 'utf-8')),
-    redeemedMetaEvents: JSON.parse(readFileSync(`./scripts/events/redeemedMeta.json`, 'utf-8')),
-    sweptEvents: JSON.parse(readFileSync(`./scripts/events/swept.json`, 'utf-8')),
+    depositedEvents: JSON.parse(
+      readFileSync(`./scripts/events/deposited.json`, "utf-8")
+    ),
+    borrowedEvents: JSON.parse(
+      readFileSync(`./scripts/events/borrowed.json`, "utf-8")
+    ),
+    repaidEvents: JSON.parse(
+      readFileSync(`./scripts/events/repaid.json`, "utf-8")
+    ),
+    withdrawnEvents: JSON.parse(
+      readFileSync(`./scripts/events/withdrawn.json`, "utf-8")
+    ),
+    redeemedEvents: JSON.parse(
+      readFileSync(`./scripts/events/redeemed.json`, "utf-8")
+    ),
+    redeemedMetaEvents: JSON.parse(
+      readFileSync(`./scripts/events/redeemedMeta.json`, "utf-8")
+    ),
+    sweptEvents: JSON.parse(
+      readFileSync(`./scripts/events/swept.json`, "utf-8")
+    ),
   };
 
   return events;
@@ -27,29 +41,32 @@ function readEventJsonFiles(): EventData {
 const events = readEventJsonFiles();
 // console.log(events);
 const extractedEvents = Object.values(events).reduce((acc, eventArray) => {
-  eventArray.forEach(event => {
+  eventArray.forEach((event) => {
     const address = event.args[0];
     if (!acc[address]) {
-      acc[address] = []
+      acc[address] = [];
     }
     const eventName = event.event;
-    if (eventName === 'Deposited' || eventName === 'Borrowed' || eventName === 'Repaid' || eventName === 'Withdrawn') {
+    if (
+      eventName === "Deposited" ||
+      eventName === "Borrowed" ||
+      eventName === "Repaid" ||
+      eventName === "Withdrawn"
+    ) {
       acc[address].push({
         blockNumber: event.blockNumber,
         event: event.event,
-        args: BigInt(event.args[1].hex).toString()
+        args: BigInt(event.args[1].hex).toString(),
       });
     }
   });
   return acc;
 }, {});
 
-
 try {
   const jsonContent = JSON.stringify(extractedEvents, null, 2);
-  writeFileSync('./scripts/events/extractedEvents.json', jsonContent, 'utf-8');
-  console.log('結果がjsonファイルに保存されました。');
+  writeFileSync("./scripts/events/extractedEvents.json", jsonContent, "utf-8");
+  console.log("結果がjsonファイルに保存されました。");
 } catch (error) {
-  console.error('ファイルの保存中にエラーが発生しました:', error);
+  console.error("ファイルの保存中にエラーが発生しました:", error);
 }
-
