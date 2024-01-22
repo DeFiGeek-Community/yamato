67c875ac92313540ae925d2f091d4c80ce17cba3
naizo10 <naizo018767@gmail.com>
2024-01-22 15:27:55 +0900

    Update YamatoRepayer to include PledgeLib in runUpgrade

--- upgrade/mods/001_upgrade_YamatoRepayer.ts
+++ upgrade/mods/001_upgrade_YamatoRepayer.ts
@@ -3,5 +3,5 @@ import { runUpgrade } from "../../src/upgradeUtil";
 const IMPL_NAME_BASE = "YamatoRepayer";
 
 export default async function main() {
-  await runUpgrade(IMPL_NAME_BASE);
+  await runUpgrade(IMPL_NAME_BASE, ["PledgeLib"]);
 }
