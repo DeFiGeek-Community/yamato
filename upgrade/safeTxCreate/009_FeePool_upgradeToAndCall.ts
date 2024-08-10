41e39171c8df6587699f3cc90a26cfb4175a8885
naizo10 <naizo018767@gmail.com>
2024-08-10 21:50:58 +0900

    Add new upgrade scripts for Yamato modules

---
+++ upgrade/safeTxCreate/009_FeePool_upgradeToAndCall.ts
@@ -0,0 +1,5 @@
+import main from "../mods/209_FeePool_upgradeToAndCall";
+
+main().catch((e) => {
+  console.error("An error occurred:", e);
+});
---
+++ upgrade/safeTxCreate/011_PriorityRegistry_upgradeTo.ts
@@ -0,0 +1,5 @@
+import main from "../mods/211_PriorityRegistry_upgradeTo";
+
+main().catch((e) => {
+  console.error("An error occurred:", e);
+});
