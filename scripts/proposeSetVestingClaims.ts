cfe73b721d2af5f2571299df6b3463bb5cc77588
naizo10 <naizo018767@gmail.com>
2024-08-28 13:13:36 +0900

    Add proposeSetVestingClaims script for transactions

---
+++ scripts/proposeSetVestingClaims.ts
@@ -0,0 +1,33 @@
+import { readDeploymentAddress } from "../src/addressUtil";
+import { genABI } from "../src/genABI";
+import { createAndProposeTransaction } from "../src/safeUtil";
+import { readFileSync } from "fs";
+import { BigNumber } from "ethers";
+
+async function main() {
+  // YmtVestingコントラクトの名前ベース
+  const IMPL_NAME_BASE = "YmtVesting";
+  const CONTRACT_ADDRESS = readDeploymentAddress(IMPL_NAME_BASE);
+  const CONTRACT_ABI = genABI(IMPL_NAME_BASE);
+
+  // TokenDistributions.jsonから配布データを読み込む
+  const distributionsJson = readFileSync("./scripts/events/TokenDistributions.json", "utf8");
+  const distributions = JSON.parse(distributionsJson).distributions;
+
+  // 配布データをアドレスと金額の配列に変換
+  const addresses = distributions.map((distribution: any) => distribution.address);
+  const amounts = distributions.map((distribution: any) => BigNumber.from(distribution.distributedTokensBigNumber).toString());
+
+  // createAndProposeTransaction関数を使用してトランザクションを作成し、提案する
+  await createAndProposeTransaction(
+    CONTRACT_ADDRESS,
+    CONTRACT_ABI,
+    "setMultipleClaimAmounts",
+    [addresses, amounts]
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
