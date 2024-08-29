f51ca42606742b77fda7fd71ed28f13df3e0fed5
naizo10 <naizo018767@gmail.com>
2024-08-29 23:53:30 +0900

    Add conditional execution in YamatoDepositor upgrade

--- upgrade/mods/205_YamatoDepositor_upgradeTo.ts
+++ upgrade/mods/205_YamatoDepositor_upgradeTo.ts
@@ -1,6 +1,8 @@
-import { readDeploymentAddress } from "../..//src/addressUtil";
+import { readDeploymentAddress } from "../../src/addressUtil";
 import { genABI } from "../../src/genABI";
 import { createAndProposeTransaction } from "../../src/safeUtil";
+import { executeTransaction } from "../../src/upgradeUtil";
+
 const IMPL_NAME_BASE = "YamatoDepositor";
 const version = "V3";
 async function main() {
@@ -13,13 +15,20 @@ async function main() {
   const implAddress = readDeploymentAddress(IMPL_NAME_BASE, "UUPSImpl");
   if (!implAddress) return console.log("not UUPSImpl");
 
-  // createAndProposeTransaction関数を使用してトランザクションを作成し、提案する
-  await createAndProposeTransaction(
-    CONTRACT_ADDRESS,
-    CONTRACT_ABI,
-    "upgradeTo",
-    [implAddress]
-  );
+  if (process.env.NETWORK === "localhost") {
+    // executeTransaction関数を使用して任意のメソッドを実行
+    await executeTransaction(CONTRACT_ADDRESS, CONTRACT_ABI, "upgradeTo", [
+      implAddress,
+    ]);
+  } else {
+    // createAndProposeTransaction関数を使用してトランザクションを作成し、提案する
+    await createAndProposeTransaction(
+      CONTRACT_ADDRESS,
+      CONTRACT_ABI,
+      "upgradeTo",
+      [implAddress]
+    );
+  }
 }
 
 // main();
