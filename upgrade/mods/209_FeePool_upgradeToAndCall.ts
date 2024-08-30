58dd958ff8593b518d2c50aa0d417e3756dac06e
naizo10 <naizo018767@gmail.com>
2024-08-30 13:22:17 +0900

    Add network condition for transaction execution

--- upgrade/mods/209_FeePool_upgradeToAndCall.ts
+++ upgrade/mods/209_FeePool_upgradeToAndCall.ts
@@ -1,9 +1,11 @@
 import { ethers } from "ethers";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
-import { readDeploymentAddress } from "../..//src/addressUtil";
+import { readDeploymentAddress } from "../../src/addressUtil";
 
 import { genABI } from "../../src/genABI";
 import { createAndProposeTransaction } from "../../src/safeUtil";
+import { executeTransaction } from "../../src/upgradeUtil";
+
 const IMPL_NAME_BASE = "FeePool";
 const version = "V2";
 async function main() {
@@ -31,13 +33,23 @@ async function main() {
   );
   console.log(packedBytes);
 
-  // createAndProposeTransaction関数を使用してトランザクションを作成し、提案する
-  await createAndProposeTransaction(
-    CONTRACT_ADDRESS,
-    CONTRACT_ABI,
-    "upgradeToAndCall",
-    [implAddress, packedBytes]
-  );
+  if (process.env.NETWORK === "localhost") {
+    // executeTransaction関数を使用して任意のメソッドを実行
+    await executeTransaction(
+      CONTRACT_ADDRESS,
+      CONTRACT_ABI,
+      "upgradeToAndCall",
+      [implAddress, packedBytes]
+    );
+  } else {
+    // createAndProposeTransaction関数を使用してトランザクションを作成し、提案する
+    await createAndProposeTransaction(
+      CONTRACT_ADDRESS,
+      CONTRACT_ABI,
+      "upgradeToAndCall",
+      [implAddress, packedBytes]
+    );
+  }
 }
 
 // main();
