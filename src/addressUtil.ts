b974737345f0ea6820ba8f9f5b16a545324b5287
naizo10 <naizo018767@gmail.com>
2024-10-29 14:55:36 +0900

    Add optional additional directory to addressUtil functions

--- src/addressUtil.ts
+++ src/addressUtil.ts
@@ -2,29 +2,41 @@ import { readFileSync, writeFileSync, existsSync } from "fs";
 
 function _getDeploymentAddressPathWithTag(
   contractName: string,
-  tag: string
+  tag: string,
+  additionalDir?: string
 ): string {
-  return `./deployments/${process.env.NETWORK}/${contractName}${tag}`;
+  return additionalDir
+    ? `./deployments/${process.env.NETWORK}/${additionalDir}/${contractName}${tag}`
+    : `./deployments/${process.env.NETWORK}/${contractName}${tag}`;
 }
 
-export function getDeploymentAddressPath(contractName: string): string {
-  return _getDeploymentAddressPathWithTag(contractName, "");
+export function getDeploymentAddressPath(
+  contractName: string,
+  additionalDir?: string
+): string {
+  return _getDeploymentAddressPathWithTag(contractName, "", additionalDir);
 }
 
 export function getDeploymentAddressPathWithTag(
   contractName: string,
-  tag: string
+  tag: string,
+  additionalDir?: string
 ): string {
-  return _getDeploymentAddressPathWithTag(contractName, tag);
+  return _getDeploymentAddressPathWithTag(contractName, tag, additionalDir);
 }
 
 // アドレスをファイルに書き込む関数
 export function writeDeploymentAddress(
   contractName: string,
   tag: string = "",
-  address: string
+  address: string,
+  additionalDir?: string
 ): void {
-  const filePath = _getDeploymentAddressPathWithTag(contractName, tag);
+  const filePath = _getDeploymentAddressPathWithTag(
+    contractName,
+    tag,
+    additionalDir
+  );
   writeFileSync(filePath, address);
   console.log(`Address for ${contractName}${tag} written to ${filePath}`);
 }
@@ -32,9 +44,14 @@ export function writeDeploymentAddress(
 // ファイルからアドレスを読み出す関数
 export function readDeploymentAddress(
   contractName: string,
-  tag: string = ""
+  tag: string = "",
+  additionalDir?: string
 ): string {
-  const filePath = _getDeploymentAddressPathWithTag(contractName, tag);
+  const filePath = _getDeploymentAddressPathWithTag(
+    contractName,
+    tag,
+    additionalDir
+  );
   if (existsSync(filePath)) {
     const savedAddress = readFileSync(filePath, { encoding: "utf8" });
     console.log(
