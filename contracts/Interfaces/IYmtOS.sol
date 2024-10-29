09f2e144627d9855cb2f3b38ae215027dcb9157f
naizo10 <naizo018767@gmail.com>
2024-10-29 13:14:49 +0900

    Update IYmtOS interface and add IYmtOSOld interface

--- contracts/Interfaces/IYmtOS.sol
+++ contracts/Interfaces/IYmtOS.sol
@@ -10,13 +10,16 @@ pragma solidity 0.8.4;
 //solhint-disable no-inline-assembly
 
 interface IYmtOS {
-    function initialize(address _YMT, address _veYMT) external;
-
-    function addYamatoOfCurrencyOS(address _yamatoAddr) external;
-
-    function vote(address _currencyOS, address _yamato) external;
+    function initialize(address cjpyCurrencyOS) external;
 
     function YMT() external view returns (address _YMT);
 
     function veYMT() external view returns (address _veYMT);
+
+    function ymtMinter() external view returns (address _ymtMinter);
+
+    function scoreWeightController()
+        external
+        view
+        returns (address _scoreWeightController);
 }
