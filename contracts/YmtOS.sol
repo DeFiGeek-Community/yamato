5ffe34001e6591d83dc572f6698f9353caa48e38
naizo10 <naizo018767@gmail.com>
2024-10-29 13:14:40 +0900

    Refactor YmtOS contract and update dependencies

--- contracts/YmtOS.sol
+++ contracts/YmtOS.sol
@@ -12,51 +12,43 @@ pragma solidity 0.8.4;
 
 import "./veYMT.sol";
 import "./Interfaces/IYMT.sol";
-import "./Interfaces/IveYMTOld.sol";
 import "./Interfaces/IYamato.sol";
 import "./Interfaces/IYmtOS.sol";
+import "./Interfaces/ICurrencyOSV3.sol";
 import "./Dependencies/UUPSBase.sol";
-import "hardhat/console.sol";
 
 contract YmtOS is IYmtOS, UUPSBase {
     string constant YMT_SLOT_ID = "deps.YMT";
     string constant VEYMT_SLOT_ID = "deps.veYMT";
-    uint256 constant CYCLE_SIZE = 1;
+    string constant YMT_MINTER_SLOT_ID = "deps.ymtMinter";
+    string constant WEIGHT_CONTROLLER_SLOT_ID = "deps.ScoreWeightController";
 
     address[] currencyOSs;
-    mapping(address => address[]) yamatoesOfCurrencyOS;
-    address[] voters;
-    mapping(address => bool) voted;
-    mapping(address => address) decisions;
-    mapping(address => uint256) memScore;
 
-    function initialize(
-        address _YMT,
-        address _veYMT
-    ) public override initializer {
+    function initialize(address cjpyCurrencyOS) public override initializer {
         __UUPSBase_init();
+
+        currencyOSs.push(cjpyCurrencyOS);
 
         bytes32 YMT_KEY = bytes32(keccak256(abi.encode(YMT_SLOT_ID)));
         bytes32 VEYMT_KEY = bytes32(keccak256(abi.encode(VEYMT_SLOT_ID)));
+        bytes32 MINTER_KEY = bytes32(keccak256(abi.encode(YMT_MINTER_SLOT_ID)));
+        bytes32 WEIGHT_CONTROLLER_KEY = bytes32(
+            keccak256(abi.encode(WEIGHT_CONTROLLER_SLOT_ID))
+        );
+
+        address _ymt = ICurrencyOSV3(cjpyCurrencyOS).YMT();
+        address _veYMT = ICurrencyOSV3(cjpyCurrencyOS).veYMT();
+        address _ymtMinter = ICurrencyOSV3(cjpyCurrencyOS).ymtMinter();
+        address _scoreWeightController = ICurrencyOSV3(cjpyCurrencyOS)
+            .scoreWeightController();
 
         assembly {
-            sstore(YMT_KEY, _YMT)
+            sstore(YMT_KEY, _ymt)
             sstore(VEYMT_KEY, _veYMT)
+            sstore(MINTER_KEY, _ymtMinter)
+            sstore(WEIGHT_CONTROLLER_KEY, _scoreWeightController)
         }
-    }
-
-    function addYamatoOfCurrencyOS(
-        address _yamatoAddr
-    ) public override onlyCurrencyOSs {
-        // TODO: Given there exists a Yamato before adding the YmtOS contract to CurrencyOS contract, such Yamato contract won't be registered to YmtOS because the Yamato.sol doesn't know which YmtOS is should be added yet.
-        // So you need to make a new function onto CurrencyOS.sol where it can sync the existing, but unregistered yamatoes of CurrencyOS.sol.
-        /*
-            function syncYamatoesToYmtOS() onlyGovernance {
-                yamatoes.map(y=>{
-                    ymtOS.addYamatoOfCurrencyOS(address(y));
-                })
-            }
-        */
     }
 
     modifier onlyCurrencyOSs() {
@@ -75,117 +67,6 @@ contract YmtOS is IYmtOS, UUPSBase {
         currencyOSs.push(_currencyOS);
     }
 
-    function vote(
-        address _currencyOS,
-        address _yamato
-    ) public override onlyWhitelisted(_currencyOS, _yamato) {
-        decisions[msg.sender] = _yamato;
-
-        /*
-            Log all voters for only once
-        */
-        if (!voted[msg.sender]) {
-            //Note: "Once vote and transfer the veYMT" is not a DoS-vector because veYMT is not transferrable.
-            voters.push(msg.sender);
-        }
-    }
-
-    modifier onlyWhitelisted(address _currencyOS, address _yamato) {
-        address[] memory cos = yamatoesOfCurrencyOS[_currencyOS];
-        for (uint256 i = 0; i < cos.length; i++) {
-            if (cos[i] == _yamato) {
-                _;
-            }
-        }
-        revert("No yamato matched.");
-    }
-
-    function distributeYMT() public {
-        uint256 _cycle = getLastCycle();
-        uint256 _at = _cycle * CYCLE_SIZE;
-        uint256[2] memory _range = getLastCycleBlockRange();
-        uint256 _mintableInTimeframe = IveYMTOld(veYMT()).mintableInTimeframe(
-            _range[0],
-            _range[1]
-        );
-        uint256 _totalScore = 0;
-
-        /*
-            Sum up all score
-        */
-        for (uint256 i = 0; i < voters.length; i++) {
-            address _voter = voters[i];
-            address _yamato = decisions[_voter];
-            IYamato.Pledge memory _pledge = IYamato(_yamato).getPledge(_voter);
-            uint256 _score = getScore(_pledge, _at);
-            memScore[_voter] = _score;
-            _totalScore += _score;
-        }
-
-        /*
-            Calculate mintable YMT amount in this cycle
-        */
-        for (uint256 i = 0; i < voters.length; i++) {
-            address _voter = voters[i];
-            address _yamato = decisions[_voter];
-            IYamato.Pledge memory _pledge = IYamato(_yamato).getPledge(_voter);
-
-            // TODO: min((borrow*40/100)+(Totalborrow*VotingBalance/VotingTotal*(100-40)/100),borrow)*0or0.4ro0.6or0.8ro1.0
-            // Note: https://docs.google.com/document/d/1URC_h5GpBNLGQxhE2sAAhJ86taoKHkqQEwXacp8msxw/edit
-            // This document shows CJPY-denominated score result, but you still should normalize the score with "TotalScore"
-            // And then multiply that scoreShare with the
-
-            uint256 _mintThisPerson = (_mintableInTimeframe *
-                getScore(_pledge, _at)) / _totalScore;
-            IYMT(YMT()).mint(_voter, _mintThisPerson);
-            delete memScore[_voter];
-        }
-
-        /*
-            TODO: Gas compensation
-        */
-
-        // uint _amount = ???;
-        // IFeePoolV1(feePool).withdrawFromProtocol(_amount);
-    }
-
-    function getCurrentCycle() public view returns (uint256) {
-        return (block.number / CYCLE_SIZE);
-    }
-
-    function getLastCycle() public view returns (uint256) {
-        return getCurrentCycle() - 1;
-    }
-
-    function getCurrentCycleBlockRange()
-        public
-        view
-        returns (uint256[2] memory)
-    {
-        return [
-            getCurrentCycle() * CYCLE_SIZE,
-            (getCurrentCycle() + 1) * CYCLE_SIZE - 1
-        ];
-    }
-
-    function getLastCycleBlockRange() public view returns (uint256[2] memory) {
-        return [
-            getLastCycle() * CYCLE_SIZE,
-            (getLastCycle() + 1) * CYCLE_SIZE - 1
-        ];
-    }
-
-    function getScore(
-        IYamato.Pledge memory _pledge,
-        uint256 _at
-    ) public view returns (uint256) {
-        uint256 veScore = IveYMTOld(veYMT()).balanceOfAt(_pledge.owner, _at);
-
-        // TODO: Do some magic :)
-
-        return 1;
-    }
-
     function YMT() public view override returns (address _YMT) {
         bytes32 YMT_KEY = bytes32(keccak256(abi.encode(YMT_SLOT_ID)));
         assembly {
@@ -197,6 +78,29 @@ contract YmtOS is IYmtOS, UUPSBase {
         bytes32 VEYMT_KEY = bytes32(keccak256(abi.encode(VEYMT_SLOT_ID)));
         assembly {
             _veYMT := sload(VEYMT_KEY)
+        }
+    }
+
+    function ymtMinter() public view override returns (address _ymtMinter) {
+        bytes32 YMT_MINTER_KEY = bytes32(
+            keccak256(abi.encode(YMT_MINTER_SLOT_ID))
+        );
+        assembly {
+            _ymtMinter := sload(YMT_MINTER_KEY)
+        }
+    }
+
+    function scoreWeightController()
+        public
+        view
+        override
+        returns (address _scoreWeightController)
+    {
+        bytes32 WEIGHT_CONTROLLER_KEY = bytes32(
+            keccak256(abi.encode(WEIGHT_CONTROLLER_SLOT_ID))
+        );
+        assembly {
+            _scoreWeightController := sload(WEIGHT_CONTROLLER_KEY)
         }
     }
 }
