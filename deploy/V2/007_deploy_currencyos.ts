3dea128099578854537fdd4d9b76df82b76a7cbb
naizo10 <naizo018767@gmail.com>
2024-10-30 13:45:43 +0900

    Add CurrencyOS deployment script for V2

---
+++ deploy/V2/007_deploy_currencyos.ts
@@ -0,0 +1,56 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import {
+  deploy,
+  setProvider,
+  getDeploymentAddressPath,
+  getDeploymentAddressPathWithTag,
+  setNetwork,
+} from "../../src/deployUtil";
+import { readFileSync, writeFileSync, existsSync } from "fs";
+import { CurrencyOS, CurrencyOS__factory } from "../../typechain";
+import { getProxy } from "../../src/testUtil";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (existsSync(getDeploymentAddressPathWithTag("CurrencyOS", "ERC1967Proxy")))
+    return;
+
+  setNetwork(hre.network.name);
+  const p = await setProvider();
+  const { ethers, deployments } = hre;
+  const { getContractFactory } = ethers;
+
+  const currency = process.env.CURRENCY;
+
+  // CURRENCYが設定されていない場合は終了
+  if (!currency) {
+    console.error("CURRENCY environment variable is not set.");
+    return;
+  }
+
+  const cjpyAddr = readFileSync(getDeploymentAddressPath(currency)).toString();
+  const feePoolAddr = readFileSync(
+    getDeploymentAddressPath("FeePoolERC1967Proxy")
+  ).toString();
+  const feedAddr = readFileSync(
+    getDeploymentAddressPathWithTag("PriceFeedSingle", "ERC1967Proxy", currency)
+  ).toString();
+
+  const inst = await getProxy<CurrencyOS, CurrencyOS__factory>(
+    "CurrencyOS",
+    [cjpyAddr, feedAddr, feePoolAddr],
+    4
+  );
+  const implAddr = await inst.getImplementation();
+
+  writeFileSync(
+    getDeploymentAddressPathWithTag("CurrencyOS", "ERC1967Proxy", currency),
+    inst.address
+  );
+  writeFileSync(
+    getDeploymentAddressPathWithTag("CurrencyOS", "UUPSImpl", currency),
+    implAddr
+  );
+};
+export default func;
+func.tags = ["CurrencyOS_V2"];
