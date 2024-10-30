a22dfdb717c32a89f5730598ee2100a4cab8df53
naizo10 <naizo018767@gmail.com>
2024-10-30 13:45:37 +0900

    Add YmtOS V2 deployment functionality

---
+++ deploy/V2/003_deploy_ymtos.ts
@@ -0,0 +1,51 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import {
+  deploy,
+  setProvider,
+  getDeploymentAddressPath,
+  getDeploymentAddressPathWithTag,
+  setNetwork,
+} from "../../src/deployUtil";
+import { readFileSync, writeFileSync, existsSync } from "fs";
+import { CurrencyOS, CurrencyOS__factory } from "../../typechain";
+import { getProxy } from "../../src/testUtil";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (existsSync(getDeploymentAddressPathWithTag("CurrencyOS", "ERC1967Proxy")))
+    return;
+
+  setNetwork(hre.network.name);
+  const p = await setProvider();
+  const { ethers, deployments } = hre;
+  const { getContractFactory } = ethers;
+
+  const currency = process.env.CURRENCY;
+
+  // CURRENCYが設定されていない場合は終了
+  if (!currency) {
+    console.error("CURRENCY environment variable is not set.");
+    return;
+  }
+
+  const currencyosAddr = readFileSync(
+    getDeploymentAddressPathWithTag("CurrencyOS", "ERC1967Proxy")
+  ).toString();
+
+  const inst = await getProxy<CurrencyOS, CurrencyOS__factory>(
+    "YmtOS",
+    [currencyosAddr],
+  );
+  const implAddr = await inst.getImplementation();
+
+  writeFileSync(
+    getDeploymentAddressPathWithTag("YmtOS", "ERC1967Proxy"),
+    inst.address
+  );
+  writeFileSync(
+    getDeploymentAddressPathWithTag("YmtOS", "UUPSImpl"),
+    implAddr
+  );
+};
+export default func;
+func.tags = ["YmtOS_V2"];
