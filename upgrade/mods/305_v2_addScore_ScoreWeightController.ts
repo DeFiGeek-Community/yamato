3f1897e44f7d4f0eac911e3a783d659394cd5746
naizo10 <naizo018767@gmail.com>
2024-11-01 16:16:02 +0900

    Add ScoreWeightController for currency upgrade

---
+++ upgrade/mods/305_v2_addScore_ScoreWeightController.ts
@@ -0,0 +1,50 @@
+import { readDeploymentAddress } from "../../src/addressUtil";
+import { genABI } from "../../src/genABI";
+import { createAndProposeTransaction } from "../../src/safeUtil";
+import { executeTransaction } from "../../src/upgradeUtil";
+import { utils } from "ethers";
+
+const IMPL_NAME_BASE = "ScoreWeightController";
+const version = "V2";
+async function main() {
+  const currency = process.env.CURRENCY;
+
+  // CURRENCYが設定されていない場合は終了
+  if (!currency) {
+    console.error("CURRENCY environment variable is not set.");
+    return;
+  }
+
+  const implNameBase = `${IMPL_NAME_BASE}${version}`;
+  const CONTRACT_ADDRESS = readDeploymentAddress(
+    IMPL_NAME_BASE,
+    "ERC1967Proxy"
+  );
+  const CONTRACT_ABI = genABI(implNameBase);
+  const scoreRegistryAddr = readDeploymentAddress(
+    "ScoreRegistry",
+    "UUPSImpl",
+    currency
+  );
+  if (!scoreRegistryAddr) return console.log("not scoreRegistryAddr");
+
+  if (process.env.NETWORK === "localhost") {
+    // executeTransaction関数を使用して任意のメソッドを実行
+    await executeTransaction(CONTRACT_ADDRESS, CONTRACT_ABI, "addScore", [
+      scoreRegistryAddr,
+      utils.parseEther("1"),
+    ]);
+  } else {
+    // createAndProposeTransaction関数を使用してトランザクションを作成し、提案する
+    await createAndProposeTransaction(
+      CONTRACT_ADDRESS,
+      CONTRACT_ABI,
+      "addScore",
+      [scoreRegistryAddr, utils.parseEther("1")]
+    );
+  }
+}
+
+// main();
+
+export default main;
