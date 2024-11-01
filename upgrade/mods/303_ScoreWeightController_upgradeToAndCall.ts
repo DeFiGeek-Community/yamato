50acf7b27ec3c5570d7dd162a8cd92a2df485fac
naizo10 <naizo018767@gmail.com>
2024-11-01 16:14:57 +0900

    Add upgrade modules for CurrencyOS and ScoreWeightController

---
+++ upgrade/mods/303_ScoreWeightController_upgradeToAndCall.ts
@@ -0,0 +1,59 @@
+import { ethers } from "ethers";
+import { readDeploymentAddress } from "../../src/addressUtil";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+
+import { genABI } from "../../src/genABI";
+import { createAndProposeTransaction } from "../../src/safeUtil";
+import { executeTransaction } from "../../src/upgradeUtil";
+
+const IMPL_NAME_BASE = "ScoreWeightController";
+const version = "V2";
+async function main() {
+  const implNameBase = `${IMPL_NAME_BASE}${version}`;
+
+  const ScoreRegistryAddr = readDeploymentAddress(
+    "ScoreRegistry",
+    "ERC1967Proxy"
+  );
+  const CONTRACT_ADDRESS = readDeploymentAddress(
+    IMPL_NAME_BASE,
+    "ERC1967Proxy"
+  );
+  const CONTRACT_ABI = genABI(implNameBase);
+  const implAddress = readDeploymentAddress(IMPL_NAME_BASE, "UUPSImpl");
+  if (!implAddress) return console.log("not UUPSImpl");
+
+  const number = await time.latest();
+  const packedBytes = ethers.utils.defaultAbiCoder.encode(
+    ["address", "uint256"],
+    [ScoreRegistryAddr, Number(number)]
+  );
+
+  const functionSelector = CONTRACT_ABI.getSighash(
+    "initializeV2(address,uint256)"
+  );
+
+  const data = functionSelector + packedBytes.slice(2);
+
+  if (process.env.NETWORK === "localhost") {
+    // executeTransaction関数を使用して任意のメソッドを実行
+    await executeTransaction(
+      CONTRACT_ADDRESS,
+      CONTRACT_ABI,
+      "upgradeToAndCall",
+      [implAddress, data]
+    );
+  } else {
+    // createAndProposeTransaction関数を使用してトランザクションを作成し、提案する
+    await createAndProposeTransaction(
+      CONTRACT_ADDRESS,
+      CONTRACT_ABI,
+      "upgradeToAndCall",
+      [implAddress, data]
+    );
+  }
+}
+
+// main();
+
+export default main;
