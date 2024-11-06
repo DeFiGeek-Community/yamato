e491ce666c406b02cf9a019781fdeaaf936c4be3
naizo10 <naizo018767@gmail.com>
2024-11-06 13:25:55 +0900

    Update import paths in v2-update-safePropose.ts

--- upgrade/batches/v2-update-safePropose.ts
+++ upgrade/batches/v2-update-safePropose.ts
@@ -9,9 +9,10 @@ import { _import } from "./importUtil";
 async function main() {
   await _import("../mods/302_CurrencyOS_upgradeTo");
   await _import("../mods/303_ScoreWeightController_upgradeToAndCall");
-  await _import("../mods/304_v2_CurrencyOS_addYamato");
+  await _import("../mods/304_v2_setYmtOS_CurrencyOS");
   await _import("../mods/305_v2_addScore_ScoreWeightController");
-  await _import("../mods/306_v2_acceptGovernance");
+  await _import("../mods/306_v2_YmtOS_acceptGovernance");
+  await _import("../mods/307_v2_acceptGovernance");
 }
 
 main();
