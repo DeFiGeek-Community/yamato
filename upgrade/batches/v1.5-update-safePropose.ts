4a2e3b4fd0afb4768cf875f1b8b9d14422308111
naizo10 <naizo018767@gmail.com>
2024-12-13 14:25:19 +0900

    Remove localTest.ts and update safePropose.ts

--- upgrade/batches/v1.5-update-safePropose.ts
+++ upgrade/batches/v1.5-update-safePropose.ts
@@ -14,9 +14,6 @@
 import { _import } from "./importUtil";
 
 async function main() {
-  if (process.env.NETWORK === "localhost") {
-    return;
-  }
   await _import("../mods/201_YamatoRepayer_upgradeTo");
   await _import("../mods/202_YamatoRedeemer_upgradeTo");
   await _import("../mods/203_YamatoWithdrawer_upgradeTo");
@@ -28,6 +25,7 @@ async function main() {
   await _import("../mods/209_FeePool_upgradeToAndCall");
   await _import("../mods/230_Yamato_setScoreRegistry");
   await _import("../mods/231_FeePool_setVeYMT");
+  await _import("../mods/232_CurrencyOS_setAddress");
 }
 
 main();
