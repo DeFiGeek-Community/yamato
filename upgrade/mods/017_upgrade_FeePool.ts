134b3ef6f0d663e6f0d57be3a342eebdb17aabf8
naizo10 <naizo018767@gmail.com>
2024-12-16 13:31:26 +0900

    Update initializeV2 to start after 3 months

--- upgrade/mods/017_upgrade_FeePool.ts
+++ upgrade/mods/017_upgrade_FeePool.ts
@@ -19,8 +19,14 @@ export default async function main() {
   const _YMTAddr = readFileSync(getDeploymentAddressPath("YMT")).toString();
   const YMT = new Contract(_YMTAddr, genABI("YMT"), p);
   const startTime = await YMT.startTime();
+  const threeMonthsInSeconds = 3 * 30 * 24 * 60 * 60;
+  const startTimePlusThreeMonths = startTime.add(threeMonthsInSeconds);
+  console.log(Number(startTime))
+  console.log(Number(startTimePlusThreeMonths))
   await runUpgrade(IMPL_NAME_BASE, [], {
-    call: { fn: "initializeV2", args: [startTime] },
+    call: { fn: "initializeV2", args: [startTimePlusThreeMonths] },
   });
   console.log("log: upgrade_FeePool.ts: upgrade executed.");
 }
+
+// main();
