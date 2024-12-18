c4978304e6b76bb85055072baa0f4c8b1e1a01fc
naizo10 <naizo018767@gmail.com>
2024-12-18 13:40:26 +0900

    Remove localhost check from upgrade batches

--- upgrade/batches/v2-update-check.ts
+++ upgrade/batches/v2-update-check.ts
@@ -7,9 +7,6 @@
 import { _import } from "./importUtil";
 
 async function main() {
-  if (process.env.NETWORK !== "localhost") {
-    return;
-  }
   await _import("../mods/310_check_impl");
   await _import("../mods/311_check_setAddress");
   await _import("../mods/312_check_governance");
--- upgrade/batches/v2-update-deployImpl.ts
+++ upgrade/batches/v2-update-deployImpl.ts
@@ -7,10 +7,6 @@
 import { _import } from "./importUtil";
 
 async function main() {
-  if (process.env.NETWORK !== "localhost") {
-    console.log("not localhost");
-    return;
-  }
   await _import("../mods/300_v2_deployImpl_CurrencyOS");
   await _import("../mods/301_v2_deployImpl_ScoreWeightController");
 }
