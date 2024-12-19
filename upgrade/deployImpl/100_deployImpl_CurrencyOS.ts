a80ca83e2fbe09763c703fd4918795877d7144f7
naizo10 <naizo018767@gmail.com>
2024-12-19 12:22:12 +0900

    Add CurrencyOS and ScoreWeightController deployment

---
+++ upgrade/deployImpl/100_deployImpl_CurrencyOS.ts
@@ -0,0 +1,5 @@
+import main from "../mods/300_v2_deployImpl_CurrencyOS";
+
+main().catch((e) => {
+  console.error("An error occurred:", e);
+});
---
+++ upgrade/deployImpl/101_deployImpl_ScoreWeightController.ts
@@ -0,0 +1,5 @@
+import main from "../mods/301_v2_deployImpl_ScoreWeightController";
+
+main().catch((e) => {
+  console.error("An error occurred:", e);
+});
