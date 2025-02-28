4bbd380eb1296dc7d731ead846268acb92cfbfc9
naizo10 <76167545+naizo01@users.noreply.github.com>
2025-02-28 12:37:28 +0900

    Update executeTransaction to use localhost admin private key and specific RPC endpoint

--- src/upgradeUtil.ts
+++ src/upgradeUtil.ts
@@ -296,15 +296,17 @@ export async function executeTransaction(
   args: any[] = []
 ) {
   // .envからDEPLOYER_PRIVATE_KEYを読み込む
-  const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY;
-  if (!deployerPrivateKey) {
+  const adminPrivateKey = process.env.LOCALHOST_ADMIN_PRIVATE_KEY;
+    if (!adminPrivateKey) {
     console.error("DEPLOYER_PRIVATE_KEY is not defined in .env file");
     return;
   }
 
   // JsonRpcProviderと秘密鍵からWalletを生成し、サイナーとして使用
-  const provider = new ethers.providers.JsonRpcProvider();
-  const signer = new ethers.Wallet(deployerPrivateKey, provider);
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:8545/"
+  );
+  const signer = new ethers.Wallet(adminPrivateKey, provider);
 
   const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
