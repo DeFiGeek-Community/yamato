2e3e2f70db6902ad410dbfe987db3567d212ba2d
naizo10 <76167545+naizo01@users.noreply.github.com>
2025-04-13 12:58:11 +0900

    Comment out imports in safePropose, add safePropose2

--- upgrade/batches/v1.5-update-safePropose.ts
+++ upgrade/batches/v1.5-update-safePropose.ts
@@ -23,9 +23,9 @@ async function main() {
   await _import("../mods/207_CurrencyOS_upgradeTo");
   await _import("../mods/208_Yamato_upgradeTo");
   await _import("../mods/209_FeePool_upgradeToAndCall");
-  await _import("../mods/230_Yamato_setScoreRegistry");
-  await _import("../mods/231_FeePool_setVeYMT");
-  await _import("../mods/232_CurrencyOS_setAddress");
+  // await _import("../mods/230_Yamato_setScoreRegistry");
+  // await _import("../mods/231_FeePool_setVeYMT");
+  // await _import("../mods/232_CurrencyOS_setAddress");
 }
 
 main();
